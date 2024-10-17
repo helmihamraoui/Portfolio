9b723c4e35fbcd993297e5baf0b0e07f9df3df99
wided msakem <widedmsakem@gmail.com>
2024-10-17 09:52:12 +0100

    index

--- script.js
+++ script.js
@@ -1,60 +1,14 @@
-
-var myTimeout = setTimeout(myGreeting, 2500);
-function myGreeting() {
-    document.getElementById('developement').src="image/desc.jpg"
+console.log("page loaded...");
+function change(id){
+    var element=document.querySelector(id) ;
+    element.innerText = "Jeremy Benth";
 }
-var Timeout = setTimeout(myStopFunction, 5000);
-function myStopFunction() {
-    document.getElementById('developement').src="image/web_developer.webp"
+function deleteThis(id){
+    var element = document.querySelector(id);
+    element.remove() ;
 }
-function toplanguages(element){
-var image = document.getElementById('imageskills')
-if(element.innerHTML=="front-end"){
-    document.getElementById("p1").style.color = "black";
-    document.getElementById("p3").style.color = '#e05d1c';
-document.getElementById("p2").style.color = '#e05d1c';
-image.src="image/frontend.jpg"
-}else if(element.innerHTML=="Back-end"){
-image.src="image/backend.png"
-document.getElementById("p2").style.color = "black";
-document.getElementById("p1").style.color = '#e05d1c';
-document.getElementById("p3").style.color = '#e05d1c';
-}
-else if(element.innerHTML=="Data-base"){
-image.src="image/1702882171341.jpg"
-document.getElementById("p3").style.color = "black";
-document.getElementById("p1").style.color = '#e05d1c';
-document.getElementById("p2").style.color = '#e05d1c';
-}
-}
-function startvideo(element){
-    element.play()
-}
-function closevideo(element){
-    element.pause()
-}
-function linkopen(id){
-    if(id=='dojoweather'){
-        location.assign("https://github.com/helmihamraoui/Web_Fundamentals/tree/main/Week_2/Day_2/Practice/DojoWeather")
-    }else if(id=='page'){
-        location.assign("https://github.com/helmihamraoui/Web_Fundamentals/tree/main/Week_2/Day_1/Core/Button%20Clicker")
-    }else if(id=='vedio'){
-        location.assign("https://github.com/helmihamraoui/Web_Fundamentals/tree/main/Week_2/Day_1/Practice/Video%20Preview")
-    }else if(id=='likes'){
-        location.assign("https://github.com/helmihamraoui/Web_Fundamentals/tree/main/Week_2/Day_2/Core/Liking%20Likes/partie2")
-    }
+function increase(x) {
+    var element = document.querySelector(".badge") ;
+    element.innerText=parseInt(element.innerText)+1 ; 
 
 }
-function zoomin(element){
-    element.style.width='750px';
-    element.style.transition ="all 2s";
-}
-function defult(element){
-    element.style.width='650px';
-}
-document.getElementById("cv")
-    .addEventListener("click", function () {
-        var text = document.getElementById("text").value;
-        var filename = "output.txt";
-        download(filename, text);
-    }, false);
